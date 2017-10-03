45f5389921af634d2572267b20da3dc7b897ff68
giorgia <g.sambrotta@gmail.com>
2017-10-03 15:39:52 +0200

    fix api url and maps key

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill'
 
 const API_KEY = 'ba4af820f923818f01116860c45029cb';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
